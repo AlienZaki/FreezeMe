f3ae9ac5fd6f5eade06fb390850884800b37ce99
alien <eng@zaki.com>
2022-05-11 18:22:56 +0200

    change newscraper to FreezeMe

--- FreezeMe/settings.py
+++ FreezeMe/settings.py
@@ -159,12 +159,12 @@ if DEBUG is False:
     # s3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'newsscraper.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'FreezeMe.storage_backends.StaticStorage'
 
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'newsscraper.storage_backends.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'FreezeMe.storage_backends.PublicMediaStorage'
 
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
