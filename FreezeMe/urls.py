2f440f992ffa888aef98b91bdd9a8857a4b2e08b
alien <eng@zaki.com>
2022-05-11 19:52:35 +0200

    fix static try 7

--- FreezeMe/urls.py
+++ FreezeMe/urls.py
@@ -3,6 +3,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core.views import ClientsItemListView, SubmissionsRecordListView, SettingsListView, NewClientListView
+from django.http import JsonResponse
+from FreezeMe.settings import BASE_DIR, STATIC_ROOT, MEDIA_ROOT, STATICFILES_DIRS
+
+
+def test(request):
+    data = {
+        'BASE_DIR': BASE_DIR,
+        'STATIC_ROOT': STATIC_ROOT,
+        'MEDIA_ROOT': MEDIA_ROOT,
+        'STATICFILES_DIRS': STATICFILES_DIRS
+    }
+    return JsonResponse(data)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +22,8 @@ urlpatterns = [
     path('submissions/history/', SubmissionsRecordListView.as_view(), name='submissions-history'),
     path('settings/', SettingsListView.as_view(), name='settings'),
     path('client/new/', NewClientListView.as_view(), name='new-client'),
+
+    path('test/', test),
 
 
     # Debug Toolbar
