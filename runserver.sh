ec6853f7eaf61a10dcbf0e62221eb28beba11a8e
alien <eng@zaki.com>
2022-05-11 18:08:53 +0200

    add collectstatic

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py loaddata users websites settings clients requirements submissions states settings
 gunicorn FreezeMe.wsgi --bind=0.0.0.0:80
