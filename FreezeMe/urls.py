5cad907b73f639fb0b5ae70cc38df01eeb5bcd6b
alien <eng@zaki.com>
2022-05-24 17:24:27 +0200

    ...

--- FreezeMe/urls.py
+++ FreezeMe/urls.py
@@ -20,8 +20,6 @@ urlpatterns = [
     path('test/', views.test, name='test'),
 ]
 
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
