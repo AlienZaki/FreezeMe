9539a4517e609cedb2fb87cf4d362a811af784d4
alien <eng@zaki.com>
2022-05-25 16:35:21 +0200

    ...

--- runserver.sh
+++ runserver.sh
@@ -2,5 +2,5 @@
 
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
-python manage.py loaddata users websites settings clients requirements states settings
+python manage.py loaddata users websites settings states
 gunicorn FreezeMe.wsgi --bind=0.0.0.0:80
