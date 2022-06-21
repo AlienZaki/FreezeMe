4be9ac871e2ea4a3a1efbe81262fa45c1a55f569
alien <eng@zaki.com>
2022-06-22 01:59:29 +0200

    normal text captcha

--- core/automation/utils/captcha.py
+++ core/automation/utils/captcha.py
@@ -1,3 +1,7 @@
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FreezeMe.settings")
+django.setup()
+
 from twocaptcha import TwoCaptcha
 from twocaptcha.api import ApiException
 from core.models import Settings
@@ -25,6 +29,23 @@ class CaptchaSolver:
             raise Exception(errors[e.__str__()])
         return result
 
+    def solve_normal_captcha(self, image_url):
+        try:
+            print('=> Solving captcha...')
+            result = self.solver.normal(image_url)
+            print('=> Captcha solved!')
+        except ApiException as e:
+            errors = {
+                'ERROR_BAD_PARAMETERS': '2Captcha: Please check the API key permissions',
+                'ERROR_WRONG_USER_KEY': '2Captcha: Invalid Key',
+                'ERROR_CAPTCHA_UNSOLVABLE': '2Captcha: Invalid site URL',
+                'ERROR_WRONG_GOOGLEKEY': '2Captcha: Wrong google site key',
+            }
+            raise Exception(errors[e.__str__()])
+        return result
+
 
 if __name__ == '__main__':
-    CaptchaSolver()
+    c = CaptchaSolver()
+    x = c.solve_normal_captcha('https://www.chexsystems.com/web/PA_ChexSystemsDotCom/CaptchaLoads.gif?1655817259651')
+    print(x)
