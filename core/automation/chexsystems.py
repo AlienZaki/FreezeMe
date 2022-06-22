a359402d471e1c610e0109d0d9490cc99dfc435a
alien <eng@zaki.com>
2022-06-22 22:18:28 +0200

    chromedriver

--- core/automation/chexsystems.py
+++ core/automation/chexsystems.py
@@ -6,9 +6,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
-import os
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
 class Chexsystems:
@@ -39,7 +41,30 @@ class Chexsystems:
         # opt.add_experimental_option('useAutomationExtension', False)
         # opt.add_experimental_option("windowTypes", ["webview"])
 
-        self.driver = webdriver.Chrome(options=opt, service=Service(chromedriver_autoinstaller.install()))  #'/usr/local/bin/chromedriver'
+        # FireFox binary path (Must be absolute path)
+        FIREFOX_BINARY = FirefoxBinary('/opt/firefox/firefox')
+
+        # FireFox PROFILE
+        PROFILE = webdriver.FirefoxProfile()
+        PROFILE.set_preference("browser.cache.disk.enable", False)
+        PROFILE.set_preference("browser.cache.memory.enable", False)
+        PROFILE.set_preference("browser.cache.offline.enable", False)
+        PROFILE.set_preference("network.http.use-cache", False)
+        PROFILE.set_preference("general.useragent.override",
+                               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0")
+
+        # FireFox Options
+        FIREFOX_OPTS = Options()
+        FIREFOX_OPTS.log.level = "trace"  # Debug
+        FIREFOX_OPTS.headless = True
+        GECKODRIVER_LOG = '/geckodriver.log'
+        self.driver = webdriver.Firefox(
+            firefox_binary=FIREFOX_BINARY,
+            firefox_profile=PROFILE,
+            options=FIREFOX_OPTS,
+            service_log_path=GECKODRIVER_LOG
+        )
+        #self.driver = webdriver.Chrome(options=opt, service=Service(GeckoDriverManager().install()))  #'/usr/local/bin/chromedriver'
         self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
     def submit(self, fname, mname, lname, email, ssn, phone, dob, address_line1, address_line2, zip, city,  state_abbreviation):
