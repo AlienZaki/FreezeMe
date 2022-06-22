b5febcd926daa48b33d32e38ee112a5a315280fe
alien <eng@zaki.com>
2022-06-23 01:46:38 +0200

    TelecomUtilityExchange

--- core/automation/telecomUtilityExchange.py
+++ core/automation/telecomUtilityExchange.py
@@ -25,7 +25,7 @@ class TelecomUtilityExchange:
         opt = webdriver.ChromeOptions()
         opt.add_argument('--disable-dev-shm-usage')
         opt.add_argument("--no-sandbox")    # **
-        #opt.add_argument("--headless")  # **
+        opt.add_argument("--headless")  # **
 
         opt.add_argument('--disable-blink-features=AutomationControlled')
         opt.add_argument("--disable-blink-features")
